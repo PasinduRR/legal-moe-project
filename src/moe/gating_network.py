c601e65715849de0b0d54394a92ad2a99afe3b6f
Pasindu Rangana <115542129+PasinduRR@users.noreply.github.com>
2025-07-09 11:24:04 +0530

    remove gating_network.py

--- src/moe/gating_network.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-import torch
-
-class GatingNetwork(nn.Module):
-    def __init__(self, input_dim, num_experts):
-        super().__init__()
-        self.fc = nn.Linear(input_dim, num_experts)
-    def forward(self, x):
-        return torch.softmax(self.fc(x), dim=-1)
